07eb17491ed9d463394cfb1efd133b299539891f
nehalbhojraj <nehalbhojraj.nb@gmail.com>
2023-03-02 19:20:46 +0530

    #commit consume api using restTemplate 2

--- src/main/java/com/app/thirdparty/controller/MyController.java
+++ src/main/java/com/app/thirdparty/controller/MyController.java
@@ -1,13 +1,16 @@
 package com.app.thirdparty.controller;
 
+import com.app.thirdparty.Model.Company;
 import com.app.thirdparty.Model.DetectRequest;
 import com.app.thirdparty.Model.Employee;
 import com.app.thirdparty.service.ServiceLayer;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -16,14 +19,37 @@ public class MyController {
     @Autowired
     private ServiceLayer serviceLayer;
 
-    @GetMapping("/rest")
-    public ResponseEntity<String> consumeRest(){
-        return new ResponseEntity<>(serviceLayer.consumeRest(), HttpStatus.OK);
-    }
-
     @GetMapping("/unirest")
     public Object detectLanguage(@RequestBody DetectRequest detectRequest) throws UnirestException {
         return serviceLayer.consumeApi(detectRequest.getText());
 
     }
+    //In string
+    @GetMapping("/rest")
+    public ResponseEntity<String> consumeEmployeesString(){
+        return new ResponseEntity<>(serviceLayer.consumeRest(), HttpStatus.OK);
+    }
+    //Convert String to Json and Json to String
+    @GetMapping("/rest11")
+    public ResponseEntity<String> CompanyEmplyeesInString() throws IOException {
+        return new ResponseEntity<>(serviceLayer.consumeRestAndReturnJsonAndBacktoString(), HttpStatus.OK);
+    }
+    //Convert String to Json
+    @GetMapping("/rest111")
+    public Company CompanyEmplyeesInJson() throws IOException {
+        return serviceLayer.consumeRestAndReturnJson();
+    }
+
+//    //SingleEmployeeJson
+//    @GetMapping("/rest1")
+//    public Company consumeRest1() throws JsonProcessingException {
+//        return serviceLayer.consumeRest1();
+//    }
+
+    //convert to json only data
+    @GetMapping("/rest2")
+    public Employee consumeRest2() throws JsonProcessingException {
+        return serviceLayer.consumeRest2();
+    }
+
 }
--- src/main/java/com/app/thirdparty/service/ServiceLayer.java
+++ src/main/java/com/app/thirdparty/service/ServiceLayer.java
@@ -1,12 +1,22 @@
 package com.app.thirdparty.service;
 
 
+import com.app.thirdparty.Model.Company;
 import com.app.thirdparty.Model.Employee;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class ServiceLayer {
@@ -14,8 +24,12 @@ public class ServiceLayer {
     @Autowired
     private RestTemplate restTemplate;
 
+    public ServiceLayer(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
 
+    //unirest
     public Object consumeApi(String text) throws UnirestException {
 
         Object response = Unirest.post("https://google-translate1.p.rapidapi.com/language/translate/v2/detect")
@@ -28,8 +42,62 @@ public class ServiceLayer {
         return response;
     }
 
+    //In string
     public String consumeRest(){
         return  restTemplate.getForObject("https://dummy.restapiexample.com/api/v1/employees",String.class);
+    }
 
+    //Convert String to Json and Json to String
+    public String consumeRestAndReturnJsonAndBacktoString() throws IOException {
+        // Make REST API call and get response as a String
+        String responseString = restTemplate.getForObject("https://dummy.restapiexample.com/api/v1/employees", String.class);
+
+        // Create ObjectMapper instance to convert String to JSON
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Convert String to JSON object
+        Object responseJson = mapper.readValue(responseString, Object.class);
+
+        // Convert JSON object back to String
+        String jsonResponseString = mapper.writeValueAsString(responseJson);
+
+        // Return JSON response String
+        return jsonResponseString;
+    }
+
+    public Company consumeRestAndReturnJson() throws IOException {
+
+        // Make REST API call and get response as a String
+        String responseString = restTemplate.getForObject("https://dummy.restapiexample.com/api/v1/employees", String.class);
+
+        // Create ObjectMapper instance to convert String to JSON
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Convert String to JSON object
+        Company responseJson = mapper.readValue(responseString, Company.class);
+
+        // Return JSON response String
+        return  responseJson;
+    }
+
+//    //DirectJson
+//    public Company consumeRest1() throws JsonProcessingException {
+//        String  company=  restTemplate.getForObject("https://dummy.restapiexample.com/api/v1/employee/1",String.class);
+//        ObjectMapper mapper = new ObjectMapper();
+//        Company responseJson = mapper.readValue(company, Company.class);
+//        return responseJson;
+//    }
+
+    //convert to json
+    public Employee consumeRest2() throws JsonProcessingException {
+
+        String emp = restTemplate.getForObject("https://dummy.restapiexample.com/api/v1/employee/1", String.class);
+
+        JSONObject json = new JSONObject(emp);
+        ObjectMapper mapper = new ObjectMapper();
+        Employee employee = mapper.readValue(json.get("data").toString(), Employee.class);
+
+        return employee;
     }
 }
+
